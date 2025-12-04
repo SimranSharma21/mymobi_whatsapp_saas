a7a792dfd628a78c28e7bd73ae9969aca988b061
Simran Sharma <simran.sharma@mymobiforce.com>
2025-12-04 16:50:40 +0530

    Added my_test_doctype and fixed folder structure

---
+++ mymobi_whatsapp_saas/mymobi_whatsapp_saas/doctype/my_test_doctype/my_test_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Simran Sharma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
